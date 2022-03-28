49d6c2d6bc832c21647809e13f02357e11e76fb0
Kevin Song <chips@ksong.dev>
2022-03-27 23:12:09 -0500

    More "fixes"

--- install_all.sh
+++ install_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$(realpath "$SCRIPT_DIR")"
 
 pushd "${SCRIPT_DIR}/preinstall_setup" || exit 1
 
@@ -10,19 +11,19 @@ python3 download_files.py
 popd || exit 1
 
 # Just build packages for now.
+debdir="${SCRIPT_DIR}/debs"
+mkdir -p "${debdir}"
 for lang in "${SCRIPT_DIR}/pkgbuilds/"*; do
     if [[ -d "$lang" ]]; then
         pushd "$lang" || exit 1
-        makedeb -s
+        if ls ./*.deb &>/dev/null; then
+            makedeb -s
+            mv ./*.deb "${debdir}"
+        fi
         popd || exit 1
     fi
 done
 
-# Run installation
-for lang in "${SCRIPT_DIR}/pkgbuilds/"*; do
-    if [[ -d "$lang" ]]; then
-        pushd "$lang" || exit 1
-        sudo apt-get install --force --yes ./*.deb
-        popd || exit 1
-    fi
+for deb in "${debdir}/"*; do
+    sudo apt-get install -f --yes "$deb"
 done
